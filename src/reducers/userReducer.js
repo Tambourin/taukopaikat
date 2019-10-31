636230284145e28dd6573cb9904bd9bf74fee66c
Tambourin <jirka.hartonen@gmail.com>
2019-10-31 20:08:22 +0200

    REACT_APP_ variable

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,12 +2,10 @@ import createAuth0Client from "@auth0/auth0-spa-js";
 import authConfig from "../authConfig";
 import placeService from "../services/placesService";
 
-
 const CONFIGURE_START = "CONFIGURE_START";
 const CONFIGURE_SUCCESS = "CONFIGURE_SUCCESS";
 const CONFIGURE_FAILURE = "CONFIGURE_FAILURE";
 const LOGOUT = "LOGOUT";
-
 
 const defaultState = {
   authClient: null,
@@ -18,16 +16,37 @@ const defaultState = {
   errored: false
 };
 
-const userReducer = (state=defaultState, action) => {
+const userReducer = (state = defaultState, action) => {
   switch (action.type) {
     case CONFIGURE_START:
-      return { ...state, loading: true, errored: false }
-    case CONFIGURE_SUCCESS:      
-      return { ...state, loading: false, errored: false, user: action.user, token: action.token, isAuthenticated: action.isAuthenticated };
+      return { ...state, loading: true, errored: false };
+    case CONFIGURE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        errored: false,
+        user: action.user,
+        token: action.token,
+        isAuthenticated: action.isAuthenticated
+      };
     case CONFIGURE_FAILURE:
-      return { ...state, loading: false, errored: true, isAuthenticated: false, user: null, token: null }    
+      return {
+        ...state,
+        loading: false,
+        errored: true,
+        isAuthenticated: false,
+        user: null,
+        token: null
+      };
     case LOGOUT:
-      return { ...state, user: null, token: null, isAuthenticated: null, loading: false, errored: false };    
+      return {
+        ...state,
+        user: null,
+        token: null,
+        isAuthenticated: null,
+        loading: false,
+        errored: false
+      };
     default:
       return state;
   }
@@ -39,14 +58,14 @@ export const initializeAuth = () => {
     dispatch({ type: CONFIGURE_START });
     try {
       const authClient = await createAuth0Client(authConfig());
-      if (window.location.search.includes("code=")) {        
+      if (window.location.search.includes("code=")) {
         await authClient.handleRedirectCallback();
         window.history.replaceState({}, document.title, "/");
-      }     
+      }
       const isAuthenticated = await authClient.isAuthenticated();
       let user = null;
       let token = null;
-      if(isAuthenticated) {
+      if (isAuthenticated) {
         user = await authClient.getUser();
         token = await authClient.getTokenSilently();
         placeService.setToken(token);
@@ -61,29 +80,30 @@ export const initializeAuth = () => {
       dispatch({ type: CONFIGURE_FAILURE });
       console.log(error);
     }
-  }
-}
+  };
+};
 
 export const login = () => {
-  return async (dispatch) => {
+  return async dispatch => {
     console.log("login");
     dispatch({ type: CONFIGURE_START });
     try {
       const authClient = await createAuth0Client(authConfig());
       await authClient.loginWithRedirect({
-        redirect_uri: process.env.NODE_ENV === "production"
-        ? "https://taukopaikat.herokuapp.com"
-        : "http://localhost:3000"
-      });          
-    } catch (error){
+        redirect_uri:
+          process.env.NODE_ENV === "production"
+            ? "https://taukopaikat.herokuapp.com"
+            : "http://localhost:3000"
+      });
+    } catch (error) {
       dispatch({ type: CONFIGURE_FAILURE });
       console.log(error);
     }
-  }
-}
+  };
+};
 
 export const logout = () => {
-  return async (dispatch) => {
+  return async dispatch => {
     dispatch({ type: CONFIGURE_START });
     try {
       const authClient = await createAuth0Client(authConfig());
@@ -93,12 +113,12 @@ export const logout = () => {
       dispatch({
         type: LOGOUT
       });
-    } catch(error) {
+    } catch (error) {
       dispatch({ type: CONFIGURE_FAILURE });
       console.log(error);
       return;
-    }    
-  }
-}
+    }
+  };
+};
 
 export default userReducer;
